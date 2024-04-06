d1d3ca2d24c969c4bbc3a2962f02fed2b7bff4da
Andrew Milburn <sidbell.069@gmail.com>
2024-04-06 16:52:26 +0100

    Part 3 - Main Done [p55]

---
+++ Coin_Dash/main.gd
@@ -0,0 +1,52 @@
+extends Node2D
+
+@export var coin_scene : PackedScene
+@export var playtime = 30
+
+var level = 1
+var score = 0
+var time_left = 0
+var screensize = Vector2.ZERO
+var playing = false
+
+func _ready() -> void:	# Let's get this show on the road
+	screensize = get_viewport().get_visible_rect().size
+	$Player.screensize = screensize
+	$Player.hide()
+	
+func _process(delta: float) -> void:	# Do this on a frame-by-frame basis
+	if playing and get_tree().get_nodes_in_group("coins").size() == 0:
+		level += 1
+		time_left += 5
+		spawn_coins()
+	
+func new_game():	# Now lets get this show on the road
+	playing = true
+	level = 1
+	score = 0
+	time_left = playtime
+	$Player.start()
+	$Player.show()
+	$Timer.start()
+	spawn_coins()
+	
+func spawn_coins():
+	for i in level + 4:
+		var c = coin_scene.instantiate()
+		add_child(c)
+		c.screensize = screensize
+		c.position = Vector2(randi_range(0, screensize.x), randi_range(0, screensize.y))
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
