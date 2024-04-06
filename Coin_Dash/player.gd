392544821b1cb742ef793e49323f5e56464957c4
Andrew Milburn <sidbell.069@gmail.com>
2024-04-06 09:13:26 +0100

    Part 2 Done
    Added Coin sprite & script

--- Coin_Dash/player.gd
+++ Coin_Dash/player.gd
@@ -7,7 +7,8 @@ signal hurt
 var velocity = Vector2.ZERO
 var screensize = Vector2(480, 720)
 
-func _process(delta: float) -> void:
+func _process(delta: float) -> void:	# Every Frame
+	
 	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	position += velocity * speed * delta
 	position.x = clamp(position.x, 0, 480)
@@ -21,16 +22,19 @@ func _process(delta: float) -> void:
 	if velocity.x != 0:
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 		
-func start():
+func start():	# Set stuff up
+	
 	set_process(true)
 	position = screensize / 2
 	$AnimatedSprite2D.animation = "idle"
 
-func die():
+func die():	# What happens when we hit an obstacle
+	
 	$AnimatedSprite2D.animation = "hurt"
 	set_process(false)
 
-func _on_area_entered(area: Area2D) -> void:
+func _on_area_entered(area: Area2D) -> void:	# Decide what to do when we hit something
+	
 	if area.is_in_group("coins"):
 		area.pickup()
 		pickup.emit()
