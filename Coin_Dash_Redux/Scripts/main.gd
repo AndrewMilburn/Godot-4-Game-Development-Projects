d02f3735ecd9f3f23ac68d4e3d5381415ddac769
Andrew Milburn <sidbell.069@gmail.com>
2024-04-16 09:19:17 +0100

    Added screen size monitor in Player

--- Coin_Dash_Redux/Scripts/main.gd
+++ Coin_Dash_Redux/Scripts/main.gd
@@ -1,2 +1,10 @@
 extends Node2D
 
+#var screensize:Vector2
+#var screen_size:Vector2
+
+#func _ready() -> void:
+	#screen_size = get_viewport_rect().size
+	#screensize = get_viewport().get_visible_rect().size
+	#print_debug(screen_size)
+	#print_debug(screensize)
--- Coin_Dash_Redux/Scripts/player.gd
+++ Coin_Dash_Redux/Scripts/player.gd
@@ -2,12 +2,21 @@ extends Area2D
 
 @export var speed:int
 var velocity = Vector2()
+var screensize:Vector2
+var screenwidth:int
+var screenheight:int
 
 
+func _ready() -> void:
+	screensize = get_viewport_rect().size
+	screenwidth = screensize.x
+	screenheight = screensize.y	
+	position = Vector2(screenwidth/2, screenheight/2)
+	#screensize = get_viewport_rect().size
+	print_debug(screensize)
+	
 func _process(delta):
 	
 	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down",)
 	position += velocity * speed * delta
 
-func startGame():
-	position = Vector2(100, 100)
