6dec59841f5e4ef1ec2b4a7d79a538c5af397a52
Andrew Milburn <sidbell.069@gmail.com>
2024-04-23 16:48:49 +0100

    Instruction Screen added

--- Coin_Dash_Redux/Scripts/HUD.gd
+++ Coin_Dash_Redux/Scripts/HUD.gd
@@ -5,10 +5,24 @@ signal start_game
 
 func _on_btn_start_pressed() -> void:
 	$btn_start.hide()
-	
+	$btn_instructions.hide()
+	$lbl_output.hide()
 	start_game.emit()
 	
 
 
 func _on_btn_instructions_pressed() -> void:
-	pass # Replace with function body.
+	$btn_start.hide()
+	$btn_instructions.hide()
+	$lbl_output.hide()
+	$instruction_set.show()
+	$status_container.hide()
+	$label_container.hide()
+
+func _on_btn_back_pressed() -> void:
+	$btn_start.show()
+	$btn_instructions.show()
+	$lbl_output.show()
+	$instruction_set.hide()
+	$status_container.show()
+	$label_container.show()
